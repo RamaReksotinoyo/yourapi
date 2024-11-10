d2be062973cf8062eff6ac2cca4d251cb335ceea
Rama <ramareksotinoyo@gmail.com>
2024-11-11 06:31:49 +0700

    init message gateway socketio

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ async function bootstrap() {
     new ValidationPipe(),
   );
 
+  // Aktifkan CORS
+  app.enableCors({
+    origin: true, // Sesuaikan dengan domain frontend
+    methods: ['GET', 'POST'],
+    credentials: true
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
